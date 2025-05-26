0724bbf3eddb1a152f33f252d67424409e03a1b5
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2025-05-26 22:32:56 +0530

    creating element and Dom relation

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Write your code below:
-const header = document.getElementById("header");
-header.style.backgroundColor = 'green';
-header.style.borderBottom="2px solid orange"
-const main = document.getElementById('main-heading');
-main.textContent = "Fruit World";
-main.style.color = 'orange';
-const basketHeading = document.getElementById('basket-heading');
-basketHeading.style.color = 'green';
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
+const h3 = document.createElement('h3');
+const data = document.createTextNode("Buy high quality organic fruits online");
+h3.appendChild(data);
+h3.style.fontStyle = 'italic';
+mainHeading.appendChild(h3);
 
-const thanks = document.getElementById('thanks');
-thanks.innerHTML = '<p>Please visit us again</p>';
+const divs = document.querySelectorAll('div');
+const div2 = divs[1];
+const p = document.createElement('p');
+const pData = document.createTextNode('Total fruits: 4');
+p.appendChild(pData);
+const ul = document.querySelector('.fruits');
+div2.insertBefore(p, ul);
+p.id = "fruits-total";
